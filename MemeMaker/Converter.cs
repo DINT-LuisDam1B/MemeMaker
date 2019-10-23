84d8e5ec77a3320ba5b141c2caeb121eaae60437
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-10-23 11:49:00 +0200

    Finalizar Apliacion, añadida Class Converter

---
+++ MemeMaker/Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int valor;
+
+            if (value.ToString().ToLower() == "true")
+            {
+                valor = 3;
+            }
+            else
+            {
+                valor = 0;
+            }
+
+            return valor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
